8079fe43236086e4a5fb766df9cadc25d363fd01
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-05-07 13:19:21 -0400

    updated packages

---
+++ src/main/java/fantastic/coders/tournamentManager/domain/game/model/Game.java
@@ -0,0 +1,4 @@
+package fantastic.coders.tournamentManager.domain.game.model;
+
+public class Game {
+}
--- src/main/java/fantastic/coders/tournamentManager/domain/league/model/League.java
+++ src/main/java/fantastic/coders/tournamentManager/domain/league/model/League.java
@@ -1,4 +1,7 @@
 package fantastic.coders.tournamentManager.domain.league.model;
 
+
 public class League {
+
+
 }
