b1ee5980290d449bac8b3676b4836ed2416eaf75
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-05-07 10:04:31 -0400

    first commit

---
+++ src/main/java/fantastic/coders/tournamentManager/TournamentManagerApplication.java
@@ -0,0 +1,13 @@
+package fantastic.coders.tournamentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentManagerApplication.class, args);
+	}
+
+}
